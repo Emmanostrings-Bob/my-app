8aa1c0df6296ba6364065a89d612bbef951eb251
Emmanostrings-Bob <oluwafemiayomide852@gmail.com>
2024-07-22 14:39:44 +0100

    updated

--- src/components/JoinCommunity.tsx
+++ src/components/JoinCommunity.tsx
@@ -3,7 +3,7 @@ const JoinCommunity = () => {
     return (
       <section className="py-12 bg-white text-black text-center text-2xl mb-11 mt-11">
         <h3 className="text-5xl font-bold mb-4">Ready to Frame Your Future<br/> with Photoruum?</h3>
-        <p className="mb-4 text-gray-500">Join the movement, and let's paint Africa through<br/> our lenses, one click at a time.</p>
+        <p className="mb-4 text-gray-500">Join the movement, and lets paint Africa through<br/> our lenses, one click at a time.</p>
         <p className="mb-4 font-bold ">Snap, Share, Inspire</p>
         <button className="bg-black text-white py-2 px-4 rounded-full">Join Photoruum Today!</button>
       </section>
